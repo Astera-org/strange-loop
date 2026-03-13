critical bug:  forgot that F.cross_entropy defaults to reduction, so masked crosss entropy wasn't actually masking anything.

hgattn/funcs.py
@@ -5,15 +5,14 @@ from torch.func import vmap
 from torch import Tensor
 
 
-def masked_cross_entropy(
-	pred_logit_BCM: torch.Tensor,   # float[batch, context, model]
-	targets_BC: torch.Tensor, # int[batch, context]
-	mask_BC: torch.Tensor,    # bool[batch, context]
-) -> torch.Tensor:
-
+def cross_entropy(
+	pred_logit_BCM: Tensor,
+	targets_BC: Tensor
+) -> Tensor:
 	pred_logit_BMC = pred_logit_BCM.permute(0,2,1)
-	xent_BC = F.cross_entropy(pred_logit_BMC, targets_BC)
-	return weighted_mean(xent_BC, mask_BC.to(xent_BC.dtype))
+	xent_BC = F.cross_entropy(pred_logit_BMC, targets_BC, reduction='none')
+	return xent_BC
+
 
 def kl_divergence(p: Tensor, qlog: Tensor) -> Tensor:
 	assert p.shape == qlog.shape, "p and qlog must have identical shapes"

hgattn/models/generative.py
@@ -99,10 +99,15 @@ class GenerativeModel(SimpleCompModel):
 				pred_logit_BCV = funcs.run_no_grad(self, input_BC, input_mask_BC)
 			case RunMode.MOCK:
 				pred_logit_BCV = torch.log(label_prob_BCV + 1e-15)
+
 		pred_logprob_BCV = torch.log_softmax(pred_logit_BCV, dim=2)
-		xent = funcs.masked_cross_entropy(pred_logit_BCV, label_BC, label_mask_BC)
+
+		xent_BC = funcs.cross_entropy(pred_logit_BCV, label_BC)
+		xent = funcs.weighted_mean(xent_BC, label_mask_BC.to(xent_BC.dtype))
+
 		kldiv_BC = funcs.kl_divergence(label_prob_BCV, pred_logprob_BCV).sum(axis=2)
 		kldiv = funcs.weighted_mean(kldiv_BC, label_mask_BC.to(kldiv_BC.dtype))
+
 		acc = funcs.percent_correct(pred_logit_BCV, label_BC, label_mask_BC)
 		return xent, { "percent_top_correct": acc, "kl_divergence": kldiv }
 