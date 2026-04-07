44d0fe658d6be8de2a9b81ef0fb89679807d5a7a
Henry Bigelow <hrbigelow@gmail.com>
2026-04-07 10:33:54 -0700

    working mod_addition

--- hgattn/data/mod_addition.py
+++ hgattn/data/mod_addition.py
@@ -1,3 +1,10 @@
+import equinox as eqx
+import jax
+import jax.numpy as jnp
+from jaxtyping import Key, Int, Array
+from .types import TokensAndProbs 
+from dataclasses import dataclass
+
 """
 A dataset of patterns: A B S A B S A B S ...
 in which S = (A + B) % mod_val
@@ -19,15 +26,61 @@ class ModAdditionOpts:
 
 class ModAdditionDataset(eqx.Module):
 	opts: ModAdditionOpts = eqx.field(static=True)
+	coords: jax.Array
+	distA: jax.Array
+	distBgivenA: jax.Array
 
 	def __init__(self, opts: ModAdditionOpts, is_train: bool, seed: int):
 		self.opts = opts
+		V = opts.vocab_size
+		inds = jnp.stack((jnp.arange(V * V) // V, jnp.arange(V * V) % V), axis=1)
+		key = jax.random.key(seed)
+		inds = jax.random.permutation(key, inds)
+		split = int(V * V * opts.train_split_frac) 
+		if is_train:
+			self.coords = inds[:split]
+		else:
+			self.coords = inds[split:]
+		distA = jnp.zeros(V).at[self.coords[:,0]].add(1.0) 
+		self.distA = distA / distA.sum()
+		distAB = jnp.zeros((V, V)).at[self.coords[:,0], self.coords[:,1]].add(1.0)
+		self.distBgivenA = distAB / distA[:,None]
 
-	
 	@property
 	def vocab_size(self):
 		return self.opts.vocab_size
 
 	@eqx.filter_jit
 	def _gen_item(self, key_B: Key) -> TokensAndProbs:
+		true_val, false_val = jnp.array(True), jnp.array(False)
+		
+		def b0_A(key, state, arg):
+			a, b = jax.random.choice(key, self.coords)
+			return jnp.array([1, a, b]), (a, self.distA, true_val, false_val)
+	
+		def b1_B(key, state, arg):
+			a, b = arg[:2]
+			s = (a + b) % self.opts.mod_val 
+			return jnp.array([2, s, -1]), (b, self.distBgivenA[a,:], true_val, false_val)
+
+		def b2_S(key, state, arg):
+			s = arg[0]
+			distS = jax.nn.one_hot(s, self.opts.mod_val)
+			return jnp.array([0, -1, -1]), (s, distS, true_val, true_val) 
+
+		branches = b0_A, b1_B, b2_S
+		
+		def scan_fn(carry: Int[Array, "slots"], key: Key[Array, ""]) -> tuple:
+			state, arg = carry[0], carry[1:]
+			return jax.lax.switch(state, branches, key, state, arg)
+
+		def single_scan(key, length):
+			init = jnp.array([0, -1, -1])
+			keys = jax.random.split(key, num=length)
+			return jax.lax.scan(scan_fn, init, keys)
+
+		batch_scan = jax.vmap(single_scan, in_axes=(0, None))
+		_, content = batch_scan(key_B, self.opts.context_len)
+
+		return TokensAndProbs(jax.random.key_data(key_B), *content)
 
